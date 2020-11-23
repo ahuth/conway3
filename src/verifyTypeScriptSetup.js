495d2b97964eab85f504a2ceb0816ea9f9a24073
Andrew Huth <andrew@huth.me>
2020-11-22 20:39:52 -0500

    Add workaround for https://github.com/facebook/create-react-app/issues/10117

---
+++ src/verifyTypeScriptSetup.js
@@ -0,0 +1,5 @@
+// Workaround for https://github.com/facebook/create-react-app/issues/10117, from
+// https://github.com/facebook/create-react-app/issues/9868#issuecomment-731396876.
+
+function verifyTypeScriptSetup() {}
+module.exports = verifyTypeScriptSetup;
