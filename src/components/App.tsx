2c190fd40c7af8d6d4bbcfad9751307c0c98e5f8
Andrew Huth <andrew@huth.me>
2020-11-23 17:10:36 -0500

    Implement Conway's game of life rules

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,17 @@
+import { State } from '../life/cell';
 import { create as createGeneration, from, randomize } from '../life/generation';
+import { create as createRule } from '../life/rule';
 
-const generation = randomize(createGeneration(3, 3));
-const nextGeneration = from(generation, []);
+const rules = [
+  createRule(State.inert, 3, State.alive),
+  createRule(State.alive, 0, State.inert),
+  createRule(State.alive, 1, State.inert),
+  createRule(State.alive, 2, State.alive),
+  createRule(State.alive, 3, State.alive),
+];
+
+const generation = randomize(createGeneration(10, 10));
+const nextGeneration = from(generation, rules);
 
 export default function App() {
   console.log(generation, nextGeneration);
--- src/life/rule.ts
+++ src/life/rule.ts
@@ -12,5 +12,5 @@ export function create(current: State, neighbors: number, next: State): Rule {
 
 export function apply(rules: Rule[], current: State, neighbors: number): State {
   const rule = rules.find((rule) => rule.current === current && rule.neighbors === neighbors);
-  return rule ? rule.next : current;
+  return rule ? rule.next : State.inert;
 }
