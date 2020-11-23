cc97a091a89b79be7d05f85fb767327f1c228b0f
Andrew Huth <andrew@huth.me>
2020-11-23 10:54:30 -0500

    Add mapping function for generations

--- src/life/generation.ts
+++ src/life/generation.ts
@@ -15,9 +15,23 @@ export function create(height: number, width: number): Generation {
 }
 
 export function randomize(target: Generation): Generation {
+  return map(target, random, target);
+}
+
+export function map(
+  source: Generation,
+  mapper: (curent: State, generation: Generation, row: number, col: number) => State,
+  target = create(source.length, source[0]!.length),
+): Generation {
   for (let rowIndex = 0; rowIndex < target.length; rowIndex++) {
     for (let colIndex = 0; colIndex < target[rowIndex]!.length; colIndex++) {
-      target[rowIndex]![colIndex] = random();
+      const current = source[rowIndex]![colIndex]!;
+
+      // In case the dimensions of `target` don't match `source`, check that we should set the value
+      // at the current indexes.
+      if (target[rowIndex] && typeof target[rowIndex]![colIndex] !== 'undefined') {
+        target[rowIndex]![colIndex] = mapper(current, source, rowIndex, colIndex);
+      }
     }
   }
   return target;
