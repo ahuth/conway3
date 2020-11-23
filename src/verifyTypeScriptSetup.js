b02a02a395edbc380e6136dc779fe9000134570b
Andrew Huth <andrew@huth.me>
2020-11-23 08:13:43 -0500

    Remove hack needed to workaround for react-scripts typescript bug
    https://github.com/facebook/create-react-app/issues/10117

--- src/verifyTypeScriptSetup.js
+++
@@ -1,5 +0,0 @@
-// Workaround for https://github.com/facebook/create-react-app/issues/10117, from
-// https://github.com/facebook/create-react-app/issues/9868#issuecomment-731396876.
-
-function verifyTypeScriptSetup() {}
-module.exports = verifyTypeScriptSetup;
