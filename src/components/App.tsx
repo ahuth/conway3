f1aeaea9287b00709faecfe85917c2fd923721a5
Andrew Huth <andrew@huth.me>
2020-11-27 12:55:23 -0500

    Add a slider for controlling framerate

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -7,16 +7,28 @@ const initial = randomize(create(100, 100));
 
 export default function App() {
   const [current, setCurrent] = useState(initial);
+  const [framerate, setFramerate] = useState(5);
   const [playing, togglePlaying] = useToggle(false);
 
   const step = useCallback(() => {
     setCurrent(from);
   }, []);
 
-  useFramerate(playing, 2, step);
+  useFramerate(playing, framerate, step);
 
   return (
     <div>
+      <label htmlFor="framerate-range">Framerate</label>
+      <input
+        id="framerate-range"
+        type="range"
+        min="0"
+        max="60"
+        step="1"
+        value={framerate}
+        onChange={(event) => setFramerate(Number(event.target.value))}
+      />
+      <span>{framerate}</span>
       <button
         onClick={togglePlaying}
       >
